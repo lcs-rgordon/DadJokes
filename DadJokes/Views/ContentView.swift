949a83022f8f1a175dccfdd2db27ff723d486ca8
Russell Gordon <github@russellgordon.ca>
2022-02-21 14:44:02 -0500

    Static interface completed.

--- DadJokes/Views/ContentView.swift
+++ DadJokes/Views/ContentView.swift
@@ -9,13 +9,58 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            
+            Text("How do you organize a space party? You planet.")
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .padding(30)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 16)
+                        .stroke(Color.primary, lineWidth: 4)
+                )
+                .padding(10)
+            
+            Image(systemName: "heart.circle")
+                .resizable()
+                .frame(width: 40, height: 40)
+                .foregroundColor(.secondary)
+            
+            Button(action: {
+                print("Button was pressed.")
+            }, label: {
+                Text("Another one!")
+                    .font(.title3)
+            })
+                .buttonStyle(.bordered)
+                .padding(.top, 10)
+            
+            HStack {
+                Text("Favourites")
+                    .font(.title3)
+                    .bold()
+                Spacer()
+            }
+            
+            List {
+                Text("Which side of the chicken has more feathers? The outside.")
+                Text("Why did the Clydesdale give the pony a glass of water? Because he was a little horse!")
+                Text("The great thing about stationery shops is they're always in the same place...")
+            }
+            
+            Spacer()
+                        
+        }
+        .navigationTitle("icanhazdadjoke?")
+        .padding()
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        NavigationView {
+            ContentView()
+        }
     }
 }
