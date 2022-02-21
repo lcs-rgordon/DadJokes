4a97284544ab0fee49448f206038e6dd06af8d3d
Russell Gordon <github@russellgordon.ca>
2022-02-21 14:11:12 -0500

    Added structure to model a joke retrieved from the JSON endpoint.

---
+++ DadJokes/Model/DadJoke.swift
@@ -0,0 +1,16 @@
+//
+//  DadJoke.swift
+//  DadJokes
+//
+//  Created by Russell Gordon on 2022-02-21.
+//
+
+import Foundation
+
+struct DadJoke: Decodable {
+    
+    let id: String
+    let joke: String
+    let status: Int
+    
+}
